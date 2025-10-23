fdeb7155447c48c96c733815810cadd29dc0d636
maxcomuk <alexandru.nic14@gmail.com>
2025-10-23 11:15:18 +0100

    Update Nebula Softworks.lua

--- Nebula Softworks.lua
+++ Nebula Softworks.lua
@@ -150,9 +150,9 @@ end
 local isStudio = RunService:IsStudio() or false
 local website = "nebulasoftworks.xyz/starlight"
 local Acrylic = isStudio and require(ReplicatedStorage.AcrylicBundled) or loadstring(game:HttpGet("https://raw." .. website .. "/AcrylicModule.luau"))()
-if Acrylic then
+pcall(function()
 	Acrylic.Init()
-end
+end)
 
 local Request = (syn and syn.request) or (fluxus and fluxus.request) or (http and http.request) or http_request or request
 
